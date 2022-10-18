c35fc762dfe5d6346c41eda6b7e4ec9856b334e9
HoCheol Shin <hcshin3992@gmail.com>
2022-10-18 14:53:14 +0900

    grid layout set

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -2,74 +2,14 @@
 
 const gridData = [
     {
-        bank: "웰컴저축은행",
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: "웰컴저축은행",
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
-    },
-    {
-        bank: '웰컴저축은행',
-        product: 'WELCOME 직장인사랑 보통예금',
-        rate: '연 3.5%',
-        cap: '1억원',
-        payment_date: '매월 1일'
+        rank: '1',
+        bank: "    OK저축은행                  ",
+        product: 'OK세컨드통장',
+        rate: '연 3.5% (기본 3.0% + 우대 0.5%)',
+        condition: '오픈뱅킹에  OK세컨드통장 등록',
+        cap: '1천만원',
+        paymentDate: '매 월 셋째 주 토요일 다음날',
+        method: '비대면, 영업점'
     }
 ];
 
@@ -78,36 +18,76 @@ el: document.getElementById('grid'),
 data: gridData,
 scrollX: false,
 scrollY: false,
+columnOptions: {
+    minWidth: 50
+},
 columns: [
     {
-        header: '금융사',
+        header: '순위',
+        name: 'rank',
+        minWidth: 50,
+        whiteSpace: "normal",
+        width: 'auto',
+        align: 'center'
+    },
+    {
+        header: '은행명',
         name: 'bank',
-        whiteSpace: "normal"
+        whiteSpace: "normal",
+        minWidth: 150,
+        width: 'auto'
     },
     {
-        header: '파킹통장',
+        header: '상품명',
         name: 'product',
-        whiteSpace: "normal"
+        whiteSpace: "normal",
+        //width: 'auto'
     },
     {
-        header: '최고금리',
+        header: '최고금리 (세전)',
         name: 'rate',
-        sortable: true,
-        sortingType: 'desc'
+        align: 'center',
+        width: 'auto'
+        //sortable: true,
+        //sortingType: 'desc'
+    },
+    {
+        header: '우대조건',
+        name: 'condition',
+        whiteSpace: "normal",
+        width: 'auto'
+        //whiteSpace: "normal"
+        // sortable: true,
+        // sortingType: 'desc'
     },
     {
         header: '최고금리 적용 예치금 한도',
-        name: 'cap'
+        name: 'cap',
+        align: 'center',
+        whiteSpace: "normal",
+        width: 'auto',
+        minWidth: 180
     },
     {
-        header: '이자 지급일',
-        name: 'payment_date'
+        header: '이자 지급시기',
+        name: 'paymentDate',
+        whiteSpace: "normal",
+        width: 'auto',
+        minWidth: 200,
+        align: 'center'
+    },
+    {
+        header: '가입방법',
+        name: 'method',
+        whiteSpace: "normal",
+        width: 'auto',
+        minWidth: 100
     }
 ],
-columnOptions: {
-    frozenCount: 1, // Freeze 3 left most columns and 
-    frozenBorderWidth: 2 // set the border width of frozen columns to be 2px.
-}
+// columnOptions: {
+//     frozenCount: 1, // Freeze 3 left most columns and 
+//     frozenBorderWidth: 2 // set the border width of frozen columns to be 2px.
+// }
 });
 
 tui.Grid.applyTheme('striped');
